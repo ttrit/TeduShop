7ed1f36813d58e0ade34de12428d31a98e397c23
thinh truong <thinh2it@gmail.com>
2017-03-19 16:59:02 +0700

    add domain entities for project

---
+++ TeduShop.Model/Abstract/Auditable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model.Abstract
+{
+  public abstract class Auditable : IAuditable
+  {
+    public string MetaKeyword { set; get; }
+    public string MetaDescription { set; get; }
+    public DateTime CreatedDate { set; get; }
+    [MaxLength(256)]
+    public string CreatedBy { set; get; }
+    public DateTime? UpdatedDate { set; get; }
+    public string UpdatedBy { set; get; }
+    public bool Status { set; get; }
+  }
+}
---
+++ TeduShop.Model/Abstract/IAuditable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model.Abstract
+{
+  public interface IAuditable
+  {
+    string MetaKeyword { set; get; }
+    string MetaDescription { set; get; }
+    DateTime CreatedDate { set; get; }
+    string CreatedBy { set; get; }
+    DateTime? UpdatedDate { set; get; }
+    string UpdatedBy { set; get; }
+    bool Status { set; get; }
+  }
+}
